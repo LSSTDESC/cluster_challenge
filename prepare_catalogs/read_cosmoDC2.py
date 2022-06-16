5d63fec7b9a3773281f1cb724ec4ac4d9ba5b9f0
tguillemLSST <tguillem@cca007.in2p3.fr>
2022-06-16 17:34:00 +0200

    up

--- prepare_catalogs/read_cosmoDC2.py
+++ prepare_catalogs/read_cosmoDC2.py
@@ -20,6 +20,7 @@ import pickle
 import healpy as hp
 import h5py
 import pandas as pd
+import math
 
 from opening_catalogs_functions import *
 
@@ -32,8 +33,8 @@ from clevar.match_metrics import recovery
 from clevar.match_metrics import distances
 
 #outpath
-##DC2_cat_name = 'cosmoDC2_v1.1.4'
-DC2_cat_name = 'cosmoDC2_v1.1.4_small'
+DC2_cat_name = 'cosmoDC2_v1.1.4'
+#DC2_cat_name = 'cosmoDC2_v1.1.4_small'
 
 outpath = "/sps/lsst/users/tguillem/DESC/desc_april_2022/cluster_challenge/clevar_catalogs/cosmoDC2/" + DC2_cat_name + "/"
 
@@ -42,9 +43,9 @@ if os.path.exists(outpath):
 os.makedirs(outpath)
 print('outpath = ' + outpath)
 
-#richness and mass cuts
-min_halo_mass = 10**14 #Msun
-#gc_truth: catalog objects
+#mass cut
+min_halo_mass = 10**13 #Msun
+#gc_truth: catalog object
 truth_data, gc_truth = DC2_cat_open(DC2_cat_name, min_halo_mass, cluster_only=False)
 
 #halo table
@@ -53,6 +54,10 @@ galaxy_data = truth_data[truth_data['is_central']==False]
 
 #c1 = ClCatalog('Cat1', ra=halo_data['ra'], dec=halo_data['dec'], z=halo_data['redshift'], mass=halo_data['halo_mass'], id=halo_data['halo_id'])
 c1 = Table([halo_data['halo_id'],halo_data['ra'],halo_data['dec'],halo_data['redshift'],halo_data['halo_mass']],names=('id','ra','dec','z','mass'))
+#add log(m) column to cosmoDC2
+c1.add_column(1.0, name='log_mass', index=5)
+for i in range(0,len(c1)):
+     c1['log_mass'][i]=np.log10(c1['mass'][i])
 
 #c1.add_members(id=galaxy_data['galaxy_id'], id_cluster=galaxy_data['halo_id'], ra=galaxy_data['ra'], dec=galaxy_data['dec'])
 c1_members = Table([galaxy_data['galaxy_id'],galaxy_data['halo_id'],galaxy_data['ra'],galaxy_data['dec'],galaxy_data['redshift']],names=('id','id_cluster','ra','dec','z')) 
