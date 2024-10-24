c5e9f1bb519d181ef22a05b16629b6e34d52ac96
rancesol <solomon@lapp.in2p3.fr>
2024-10-24 11:10:58 +0200

    cleared out my config and slurm output files

--- slurm_scripts/runs/EXAMPLE_RUN/matching.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#SBATCH --nodes=1
-#SBATCH --job-name=matching
-#SBATCH --time=2:00:00
-#SBATCH --partition=lsst,htc
-#SBATCH --ntasks=4
-#SBATCH --output=/pbs/throng/lsst/users/rsolomon/cluster_challenge/src/cluster_challenge//../..//slurm_outputs/runs/EXAMPLE_RUN/matching.out
-#SBATCH --mem=8GB
-python -u /pbs/throng/lsst/users/rsolomon/cluster_challenge/src/cluster_challenge/matching/matching.py runConfigs/example.cfg
